dcf87e21382068208153d0c6166c16a69f76fab0
elvedin123 <elvedinc97@gmail.com>
2023-07-28 17:46:21 -0400

    setting up home component for app

---
+++ weather-app/src/components/weatherinfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-container">
+<div class="weather-container">
+  test
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@media screen and (min-width: 480px) {
+  .container{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .weather-container{
+    width: 50%;
+    background-color: black;
+  }
+}
+</style>
