2ebb8bd7419c815f07b79a19f53a028f80a4df8b
elvedin123 <elvedinc97@gmail.com>
2023-07-31 23:19:43 -0400

    added and styled search bar

--- weather-app/src/components/SearchBox.vue
+++ weather-app/src/components/SearchBox.vue
@@ -12,22 +12,35 @@ export default {
 
 <style>
 @media screen and (max-width: 480px){
-.search-box{
-  width: 100%;
-  margin-bottom: 30px;
-  margin-top: 30px;
+    .search-box{
+      width: 100%;
+      margin-bottom: 30px;
+      margin-top: 30px;
+      
+    }
+    .search-box .search-bar{
+      display: block;
+      width: 50%;
+      padding: 5px;
+      margin-left: 80px;
+      color: #313131;
+      font-size: 15px;
+
+
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      box-shadow: 0px  0px 9px rgba(0, 0, 0, .25);
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 0px 16px 0px 16px;
+      transition: 0.4s;
+    }
+.search-box .search-bar:focus{
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
 }
-.search-box .search-bar{
-  display: block;
-  width: 50%;
-  padding: 5px;
-
-  color: #313131;
-  font-size: 15px;
-
-
-  
-}
-
 }
 </style>
