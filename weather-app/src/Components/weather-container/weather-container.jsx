1f5d50c53ec1ff8db0217b86d5087be7a9de4aaf
elvedin123 <elvedinc97@gmail.com>
2023-08-22 16:27:15 -0400

    added component styling folder for the current weather and installed a font

--- weather-app/src/Components/weather-container/weather-container.jsx
+++ weather-app/src/Components/weather-container/weather-container.jsx
@@ -3,18 +3,26 @@ import Forecast from "../Forecast/Forecast";
 const WeatherContainer = () => {
   return (
     <div className="weather-container">
-      <div className="weather">
+      <div className="current-weather">
         <div className="weather-location">
           <p>Berlin</p>
         </div>
-        <div className="weather-temp">
+        <div className="current-temp">
           <h1>30˚C</h1>
         </div>
-        <div className="weather-description">
+        <div className="current-description">
           <p>Clouds</p>
         </div>
       </div>
-      <div className="details"></div>
+      <div className="details">
+        <div className="current-feels">
+          <p>33˚C</p>
+        </div>
+        <div className="current-humidity">
+          <p>10%</p>
+        </div>
+        <div className="current-wind">5 MPH</div>
+      </div>
       <Forecast />
     </div>
   );
